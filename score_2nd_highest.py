1a418227d7ed771b8f69acc4baf8febb192eb17b
Matthew Yen <python2018wow@gmail.com>
2018-08-23 22:17:02 +0800

    find 2nd highest score

---
+++ score_2nd_highest.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+#score_2nd_highest.py
+#實作一個 function second_highest() 輸出分數為第二高的學生名字
+#不需返回值(return)
+#students 是一個二維的 list(列表)
+#若有兩個人同為第二高分則兩個名字都要輸出
+#提示: 可能會用到 list 中 sort() 的功能
+
+students = [['Petter', 89], ['Harry', 83], ['Jerry', 94], ['May', 98], ['Eric', 94]]
+
+def second_highest(students):
+
+
+
+
+second_highest(students)
